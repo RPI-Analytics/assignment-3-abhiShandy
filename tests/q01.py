bf1a7b32e66ace48d4272b9325664110fe349be4
Jason Kuruzovich <jkuruzovich@gmail.com>
2018-09-11 09:42:47 -0400

    OKpy shifting around.

---
+++ tests/q01.py
@@ -0,0 +1,38 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> fivetoten
+          [5, 6, 7, 8, 9, 10]
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> len(fivetoten)
+          6
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> fivetoten[0]
+          5
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
